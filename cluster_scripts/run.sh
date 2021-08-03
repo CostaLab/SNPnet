c0a89a00877b05144c4c6cbc4e5074285c5d96e4
Marc-Fassbender <marc@dirkfassbender.de>
2021-08-03 14:46:37 +0200

    Cluster run scripts, GPU

--- cluster_scripts/run.sh
+++ cluster_scripts/run.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
 #SBATCH --mem-per-cpu=16G
-#SBATCH --time=20:00:00
+#SBATCH --time=24:00:00
+#SBATCH --account=rwth0776
 #SBATCH -J marc.fassbender@rwth-aachen.de
-#SBATCH -o %j.out
+#SBATCH -o ./outputs/%j.out
 
-#SBATCH rwth0776
+#SBATCH --gres=gpu:1
+ 
+module load cuda
 
 source ~/anaconda3/bin/activate
 conda activate snpnet
 
-python ~/snpnet/cluster_scripts/BCE.py $1 $2
+python ~/snpnet/cluster_scripts/BCE.py $1
